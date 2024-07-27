f76d54632a2e5647c56745ec1b5f6b9edc21755f
The Berufegoru <giftmoobi@gmail.com>
2024-07-27 16:49:36 +0200

    added db models

---
+++ shared/src/db/models/adminModel.ts
@@ -0,0 +1,85 @@
+/**
+ * @fileoverview Admin model definition
+ * @module adminModel
+ */
+
+import { Association, DataTypes, Model } from 'sequelize';
+import { sequelize } from '../../libs';
+import { User } from './userModel';
+import { IAdmin } from '../../interfaces/adminInterface';
+
+/**
+ * Admin model class.
+ * @class Admin
+ * @extends {Model<IAdmin>}
+ */
+class Admin extends Model<IAdmin> implements IAdmin {
+  /**
+   * Admin ID
+   * @type {number}
+   */
+  public id!: number;
+
+  /**
+   * First name of the admin
+   * @type {string}
+   */
+  public firstName!: string;
+
+  /**
+   * Last name of the admin
+   * @type {string}
+   */
+  public lastName!: string;
+
+  /**
+   * Associated user ID
+   * @type {number}
+   */
+  public userId!: number;
+
+  /**
+   * Admin-User association
+   * @type {Association<Admin, User>}
+   */
+  public static associations: {
+    user: Association<Admin, User>;
+  };
+}
+
+Admin.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Admin',
+    timestamps: true, // Set to false if you do not need timestamps
+  }
+);
+
+// Define associations
+Admin.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+export { Admin };
---
+++ shared/src/db/models/recruiterModel.ts
@@ -0,0 +1,159 @@
+/**
+ * @fileoverview Recruiter Model Definition
+ * @version 1.0.0
+ * @module recruiterModel
+ */
+import { DataTypes, Model, Association } from 'sequelize';
+import { sequelize } from '../../libs';
+import { User } from './userModel';
+import { IRecruiter } from '../../interfaces/recruiterInterface';
+import { Job } from './jobModel';
+
+/**
+ * Recruiter model class.
+ * @class Recruiter
+ * @extends {Model<IRecruiter>}
+ */
+class Recruiter extends Model<IRecruiter> implements IRecruiter {
+  /**
+   * Recruiter ID
+   * @type {number}
+   */
+  public id!: number;
+
+  /**
+   * Recruiter Name
+   * @type {string}
+   */
+  public name!: string;
+
+  /**
+   * Industry of the Recruiter
+   * @type {string}
+   */
+  public industry!: string;
+
+  /**
+   * Website URL of the Recruiter
+   * @type {string | undefined}
+   */
+  public websiteUrl?: string;
+
+  /**
+   * Location of the Recruiter
+   * @type {string | undefined}
+   */
+  public location?: string;
+
+  /**
+   * Description of the Recruiter
+   * @type {string | undefined}
+   */
+  public description?: string;
+
+  /**
+   * Size of the Recruiter organization
+   * @type {number | undefined}
+   */
+  public size?: number;
+
+  /**
+   * Year the Recruiter was founded
+   * @type {number | undefined}
+   */
+  public foundedIn?: number;
+
+  /**
+   * Verification status of the Recruiter
+   * @type {boolean | undefined}
+   */
+  public isVerified?: boolean;
+
+  /**
+   * User ID associated with the Recruiter
+   * @type {number}
+   */
+  public userId!: number;
+
+  /**
+   * Timestamps for the Recruiter model
+   * @readonly
+   * @type {Date}
+   */
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+
+  /**
+   * Associations for the Recruiter model
+   * @static
+   * @type {object}
+   */
+  public static associations: {
+    user: Association<Recruiter, User>;
+    jobs: Association<Recruiter, Job>;
+  };
+}
+
+Recruiter.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    industry: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    websiteUrl: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    location: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    size: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    foundedIn: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    isVerified: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: true,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    modelName: 'Recruiter',
+    timestamps: true,
+  }
+);
+
+// Define associations with explicit aliases
+Recruiter.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+Recruiter.hasMany(Job, { foreignKey: 'recruiterId', as: 'jobs' });
+
+export { Recruiter };
