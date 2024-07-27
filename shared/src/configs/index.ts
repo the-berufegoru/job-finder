ddbd0cca8cc83f6f5fee908268f6746d107778a9
The Berufegoru <giftmoobi@gmail.com>
2024-07-27 11:59:12 +0200

    added shared configs

---
+++ shared/src/configs/index.ts
@@ -0,0 +1,9 @@
+/**
+ * @fileoverview
+ * @version 1.0.0
+ * @module configs
+ */
+
+export { databaseConfig } from './dbConfig';
+
+export { loggerConfig } from './loggerConfig';
---
+++ shared/src/configs/loggerConfig.ts
@@ -0,0 +1,10 @@
+/**
+ * @fileoverview
+ * @version
+ * @module loggerConfig
+ */
+import { ILoggerConfig } from '../interfaces';
+
+export const loggerConfig: ILoggerConfig = {
+  logger: 'LOGTAIL_ACCESS_TOKEN',
+};
