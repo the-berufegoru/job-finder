504249216112d90e9805189607987eb7e03e0b37
The Berufegoru <giftmoobi@gmail.com>
2024-07-27 12:00:07 +0200

    added shared dir

---
+++ shared/src/index.ts
@@ -0,0 +1,13 @@
+/**
+ * @fileoverview
+ * @module
+ * @version
+ */
+
+export * as configs from './configs';
+
+export * as interfaces from './interfaces';
+
+export * as libs from './libs';
+
+export * as utils from './utils';
