3a9f5871a0294e2e0d9ebdbe38f119473ed1e5cb
The Berufegoru <giftmoobi@gmail.com>
2024-07-31 21:29:34 +0200

    removed unused logger file

--- apps/authentication/src/utils/loggerUtil.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @fileoverview
- * @module
- * @version
- */
-import { getLoggerFor } from '@job-finder/utils';
-import { loggerConfig } from '@job-finder/configs';
-
-export const authLogger = getLoggerFor(loggerConfig?.authentication);
