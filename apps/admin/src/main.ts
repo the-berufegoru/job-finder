a9863dade1d4537924e0c0e7c46f83157c274ddb
The Berufegoru <giftmoobi@gmail.com>
2024-07-31 21:52:32 +0200

    added admin rate-limiter

--- apps/admin/src/main.ts
+++ apps/admin/src/main.ts
@@ -10,6 +10,7 @@ import AdminRoutes from './routes/admin.routes';
 import { authorizationMiddleware } from './middlewares';
 
 import dotenv from 'dotenv';
+import rateLimit from 'express-rate-limit';
 dotenv.config();
 
 const PORT: string = process.env.PORT ?? '3002';
@@ -22,8 +23,16 @@ const app: Application = express();
 
 configureMiddlewares(app);
 
+const adminRateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute window
+  max: 1000, // Limit each IP to 1000 requests per windowMs
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
 app.use(
   '/api/v1/admin',
+  adminRateLimiter,
   authorizationMiddleware.authorizeAdmin,
   new AdminRoutes().init()
 );
