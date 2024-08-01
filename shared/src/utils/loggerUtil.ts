123fd95882ba1b647ae5dcbaee0ea5fba32ca1e4
The Berufegoru <giftmoobi@gmail.com>
2024-08-01 14:40:29 +0200

    added candidate logger configs

--- shared/src/utils/loggerUtil.ts
+++ shared/src/utils/loggerUtil.ts
@@ -73,6 +73,7 @@ export const getLoggerFor: (tokenName: string) => Logger = (
 };
 
 export const logger = {
-  admin: getLoggerFor(loggerConfig?.admin),
-  auth: getLoggerFor(loggerConfig?.auth),
+  admin: getLoggerFor(loggerConfig['admin']),
+  auth: getLoggerFor(loggerConfig['auth']),
+  candidate: getLoggerFor(loggerConfig['candidate']),
 };
